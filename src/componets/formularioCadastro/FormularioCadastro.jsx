085d7269c91712d0378450bb72db10d8dd5828bd
Bernardo Ribeiro <bernardosr25@gmail.com>
2021-11-03 15:25:16 -0300

    Iniciado o processo para coletar as informações do formulário com o bind

--- src/componets/formularioCadastro/FormularioCadastro.jsx
+++ src/componets/formularioCadastro/FormularioCadastro.jsx
@@ -3,10 +3,22 @@ import React,{ Component } from "react"
 
 export class FormularioCadastro extends Component{
 
+    constructor(){
+        super();
+        this.titulo = "";
+    }
+
+    handleMudancaTitulo(evento){    
+
+        this.titulo = evento.target.value;
+        console.log(this.titulo)
+
+    }
+
     render(){
         return (
             <form className="form-cadastro">
-                <input type="text" placeholder="Título" className="form-cadastro_input" />
+                <input type="text" placeholder="Título" className="form-cadastro_input" onChange={this.handleMudancaTitulo.bind(this)} />
                 <textarea className="form-cadastro_input" rows={15} placeholder="Excreva sua nota..."></textarea>
                 <button className="form-cadastro_input form-cadastro_submit">Criar Nota</button>
             </form>
