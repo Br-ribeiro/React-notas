d11f28fe981aeafb806c971ca98419a214280abb
Bernardo Ribeiro <bernardosr25@gmail.com>
2021-11-04 18:48:39 -0300

    feito a refatoração para o trabalho com as clases separadas e render separado

--- src/componets/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/componets/ListaDeCategorias/ListaDeCategorias.jsx
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 
 class ListaDeCategorias extends React.Component {
 
+  constructor(){
+    super();
+    this.state = {categorias:[]}
+    this._novasCategorias = this._novasCategorias.bind(this);
+  }
+
+  componentDidMount(){
+    this.props.categorias.inscrever(this._novasCategorias);
+  }
+
+    _novasCategorias(categorias){
+
+      this.setState({...this.state,categorias})
+    }
+
     _handleEventoInput(evento){
         
         if(evento.key == "Enter"){
@@ -15,7 +30,7 @@ class ListaDeCategorias extends React.Component {
           <section className="lista-categorias">
 
             <ul className="lista-categorias_lista">
-                {this.props.categorias.map((categoria, index) =>{
+                {this.state.categorias.map((categoria, index) =>{
                     return <li key={index} className="lista-categorias_item">{categoria}</li>
                 })}
             </ul>
--- src/dados/Notas.js
+++ src/dados/Notas.js
@@ -2,15 +2,34 @@ export default class ArrayDeNotas{
     
     constructor(){
         this.notas = [];
+        this._inscritos = [];
     }
 
     adicionarNota(titulo, texto, categoria){
         const novaNota = new Nota(titulo,texto, categoria);
         this.notas.push(novaNota);
+        this.notificar();
     }
 
     apagarNotas(indice){
         this.notas.splice(indice,1);
+        this.notificar();
+    }
+
+    inscrever(func){
+        this._inscritos.push(func);
+    }
+
+    desinscrever(func){
+        console.log(this._inscritos.length)
+        this._inscritos = this._inscritos.filter(f => f !== func);
+        console.log(this._inscritos.length)
+    }
+    
+    notificar(){
+        this._inscritos.forEach(func => {
+            func(this.notas);
+        });
     }
 }
 
